026cab7aa21ea8919dd23a829df1594fff58541a
Olga Medvedeva <olga.medvedeva@terumobct.com>
2019-12-19 10:38:17 -0700

    test

--- src/BCT.GHActions.NuGet.Example/Constants.cs
+++ src/BCT.GHActions.NuGet.Example/Constants.cs
@@ -5,5 +5,6 @@ namespace BCT.GHActions.NuGet.Example
     public static class Constants {
         public const string Company = "Terumo BCT";
         public const string Team = "42";
+        public const string Project = "TNG";
     }
 }
